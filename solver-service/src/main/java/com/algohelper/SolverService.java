6bee075efae6466bcd678a5aeea9d751c5497b35
DeJw92 <dpawlak1992@gmail.com>
2015-11-26 10:31:58 +0100

    automatic registration in discovery service

--- solver-service/src/main/java/com/algohelper/SolverService.java
+++ solver-service/src/main/java/com/algohelper/SolverService.java
@@ -2,12 +2,13 @@ package com.algohelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Dawid Pawlak.
  */
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SolverService {
     public static void main(String[] args) {
